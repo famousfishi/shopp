2af6ad80f921caef46dd801272b5c9c280c3de66
Fisayo Oluwayemi (Dev Fishi) <fisayo@wellahealth.com>
2022-03-07 10:15:04 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopp',
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shopp'),
+      ),
+      body: const Center(
+        child: Text('Shopp, a great place'),
+      ),
+    );
+  }
+}
